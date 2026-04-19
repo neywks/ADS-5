c41287b66ccab1f20bfbcee0ee55873c641d09c6
neywks <netarly0102200@gmail.com>
2026-04-19 21:45:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,75 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int getPrior(char pr) {
+  switch (pr) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+    default:
+      return 0;
+  }
 }
 
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char> stack;
+  std::string res;
+
+  for (char ch : inf) {
+    if (std::isdigit(ch)) {
+      res += ch;
+    } else if (ch == '(') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        res += stack.pop();
+      }
+      stack.pop();
+    } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      while (true) {
+        if (stack.isEmpty()) break;
+        if (stake.get() == '(') break;
+        if (getPrior(stack.get()) < getPrior(ch)) break;
+
+        res += stack.pop();
+        }
+      stack.push(ch);
+      }
+    }
+  while (!stack.isEmpty()) {
+    res += stack.pop();
+  }
+  return res;
+}
+
+
 int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+  TStack<int> stack;
+
+  for (char ch : pref) {
+    if (std::isdigit(ch)) {
+      int num = ch - '0';
+      stack.push(num);
+    } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      int ri = stack.pop();
+      int le = stack.pop();
+
+      int res = 0;
+      if (ch == '+') {
+        res = le + ri;
+      }
+      if (ch == '-') {
+        res = le - ri;
+      }
+      if (ch == '*') {
+        res = le*ri;
+      }
+      if (ch == '/') {
+        res = le/ri;
+      }
+      stack.push(res);
+    }
+  }
+  return stuck.pop();
 }
